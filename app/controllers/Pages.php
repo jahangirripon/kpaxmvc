46ca69b3d0d8f852fc38027819d6b5f81135f1bc
riponrtbd <ripon@rtbd.com>
2019-01-31 11:26:32 +0600

    base controller methods

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,6 +1,6 @@
 <?php 
 
-class Pages {
+class Pages extends Controller {
     public function __construct() 
     {
         
@@ -8,7 +8,7 @@ class Pages {
 
     public function index()
     {
-        echo 'this is index';
+        $this->view('hello');
     }
 
     public function about($id)
---
+++ app/libraries/controller.php
@@ -0,0 +1,31 @@
+<?php
+    /* 
+    *   base controller
+    *   loads models and views
+    */
+
+    class Controller {
+        // load model
+        public function model($model)
+        {
+            // require model file
+            require_once '../app/models/' . $model . '.php';
+
+            return new $model();
+        }
+
+        //load view
+
+        public function view($view, $data = []) 
+        {
+            // check for view file
+            if(file_exists('../app/views/'. $view . '.php') ) 
+            {
+                require_once '../app/views/'. $view . '.php';
+            } else
+            {
+                // view does not exist
+                die('view does not exist');
+            }
+         }
+    }
